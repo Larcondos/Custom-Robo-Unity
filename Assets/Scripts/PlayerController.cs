d86dd89e62de5afc3e07942f09a132bef24129c2
Jake Mauk <larcondos@yahoo.com>
2018-12-20 09:07:47 -0500

    Creation of most planned "Assets" folders
    A quick start to most of the assets folders that plan on being used.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	#region Button Mapping
+	/*
+
+	Control Stick moves character in World Space
+	A Button to Jump or Air Dash
+	B Button is to Gun Attack
+	L Button is for Pod Attack
+	R Button is for Bomb Attack
+	X Button is for Charge Attack
+
+	*/
+	#endregion Button Mapping
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
