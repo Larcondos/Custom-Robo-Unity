7abbfb707ba35a253083cd5eeba4d7e00ef886d0
Jake Mauk <larcondos@yahoo.com>
2019-01-12 15:44:28 -0500

    Bullet Homing Functional

--- Assets/Scripts/Mech-Based/Weapons/BulletPath.cs
+++ Assets/Scripts/Mech-Based/Weapons/BulletPath.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletPath : MonoBehaviour {
+	
+	public Transform target;
+
+	private Rigidbody rb;
+
+	public float ATK; // Damage amount
+	public float SPD; // Speed the bullets move at
+	public float HMG; // How homing the bullets are
+	public float DWN; // How much knockback the bullets apply
+	public float RLD; // Reload Time before next shot.
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		rb.velocity = transform.forward * SPD;
+
+		var targetRotation = Quaternion.LookRotation(target.position - transform.position);
+
+		rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, HMG));
 	}
 }
