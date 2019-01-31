a47b4ff8d7fa88d155a268bf35f2ea32129ab5ec
Jake Mauk <larcondos@yahoo.com>
2019-01-31 15:01:58 -0500

    Cube Destruction is now more responsible.

---
+++ Assets/Scripts/DestroyOnTouch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTouch : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.name != "cubeShard")
+			Destroy(this.gameObject);
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name != "cubeShard")
+			Destroy (this.gameObject);
+	}
+}
