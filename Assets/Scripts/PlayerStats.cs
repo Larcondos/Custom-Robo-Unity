551d15c39440d17624e5258badb6630762a96bf6
Jake Mauk <larcondos@yahoo.com>
2019-02-12 15:03:09 -0500

    Beginning of Stat Page for early combat testing.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour {
+
+	private int HP = 1000;
+
+	// Represents the various states, 1 = ready, 2 = rebirth, 3 = downed.
+	private int state;
+
+	public Text HPText;
+	public Text stateText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
