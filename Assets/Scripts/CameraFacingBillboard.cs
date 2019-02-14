a482735430b87de0c7ab3767f74e5e5a3fc0b8c3
Jake Mauk <larcondos@yahoo.com>
2019-02-14 10:40:52 -0500

    Started Work on Top Layer UI

--- Assets/Scripts/CameraFacingBillboard.cs
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -5,9 +5,14 @@ public class CameraFacingBillboard : MonoBehaviour
 {
 	public Camera m_Camera;
 	int searching;
+	public bool topLayer;
+
+	public Material seeThru;
 
 	void Start() {
 		//m_Camera = GameObject.FindGameObjectWithTag ("Player").GetComponent<Camera> ();
+		if (topLayer)
+			GetComponent<MeshRenderer> ().material = seeThru;
 	}
 
 	void Update()
