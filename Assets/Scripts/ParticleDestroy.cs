b2f932fd1dc3a47399724275b4e1620c80ee444e
Jake Mauk <larcondos@yahoo.com>
2019-02-20 16:35:14 -0500

    More Particle Updates, added an auto death timer script based on particle duration.

---
+++ Assets/Scripts/ParticleDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestroy : MonoBehaviour {
+
+	private ParticleSystem part;
+	private float deathTimer;
+
+	// Use this for initialization
+	void Start () {
+		part = GetComponent<ParticleSystem> ();
+		deathTimer = part.main.duration;
+
+		StartCoroutine (killMe ());
+	}
+	
+	IEnumerator killMe() {
+		yield return new WaitForSeconds (deathTimer);
+
+		Destroy (this.gameObject);
+	}
+}
