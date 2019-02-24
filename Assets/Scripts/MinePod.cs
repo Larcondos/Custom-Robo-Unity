6c9408a622f2a959bce4f6daed2cc8d94c9df5b9
Jake Mauk <larcondos@yahoo.com>
2019-02-24 01:15:42 -0500

    Time bomb mine is now (mostly) functional. Needs the damage and collisions and particles...

---
+++ Assets/Scripts/MinePod.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinePod : MonoBehaviour {
+
+	private MeshRenderer meshRen;
+	private Material mat;
+
+	// Is the flash on or off?
+	private bool flashOn = false;
+
+	// How long to flash?
+	private float timer = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		meshRen = GetComponent<MeshRenderer> ();
+		mat = meshRen.material;
+		StartCoroutine (Flash ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer <= 0.03f) {
+			explode ();
+		}
+	}
+
+	void explode() {
+		print ("boom");
+		Destroy (this.gameObject);
+	}
+
+	IEnumerator Flash() {
+		if (flashOn) {
+			// Turn off emission
+			mat.DisableKeyword("_EMISSION");
+			flashOn = !flashOn;
+			print ("yeet");
+		}
+		else {
+			// Turn on emission
+			mat.EnableKeyword("_EMISSION");
+			flashOn = !flashOn;
+			print ("yote");
+		}
+		yield return new WaitForSeconds (timer);
+
+		timer *= 0.9f;
+
+		StartCoroutine (Flash ());
+	}
+}
