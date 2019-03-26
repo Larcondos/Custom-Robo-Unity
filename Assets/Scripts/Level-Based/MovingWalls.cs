e8f4eb2a7e3512b1e002f43edda4702ae4bdac25
Jake Mauk <larcondos@yahoo.com>
2019-03-26 17:47:10 -0400

    Added moving walls to the arena.

--- Assets/Scripts/Level-Based/MovingWalls.cs
+++ Assets/Scripts/Level-Based/MovingWalls.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class MovingWalls : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	// This is a boolean to determine which way the walls are moving.
+	private bool movingLeft;
+
+	// A speed multiplier at which the walls will move.
+	private float moveSpeed = 3f;
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		// If I'm moving left, go left! If not, go right.
+		if (movingLeft) {
+			goLeft ();
+		} else
+			goRight ();
 	}
+
+	// Translates to the left at a fixed rate. If it goes past a certain boundary, it switches direction.
+	void goLeft() {
+		transform.Translate (Vector3.left * Time.deltaTime * moveSpeed);
+		if (transform.position.x < -5)
+			movingLeft = false;
+	}
+
+	// Translates to the right at a fixed rate. If it goes past a certain boundary, it switches direction.
+	void goRight() {
+		transform.Translate (Vector3.right * Time.deltaTime * moveSpeed);
+		if (transform.position.x > 5)
+			movingLeft = true;
+	}
+
+
+
 }
